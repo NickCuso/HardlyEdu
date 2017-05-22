df15fd8da4947178a4a2a7fc5d1f4130ae0ecadf
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-05-21 19:26:14 -0700

    test

---
+++ CSharp6InaNutshell/GitClient/GitClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HD
+{
+  /// <summary>
+  /// On 'HardlyEdu'
+  /// 
+  /// git add --all
+  /// git commit -a -m "Test"
+  /// git push
+  /// </summary>
+  public class GitClient
+  {
+  }
+}
